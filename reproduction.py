a67114037ec354835b2f04b0d3ae3abf66bd0266
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2023-12-26 17:33:09 +0100

    Implemented 4 reproduction types

---
+++ reproduction.py
@@ -0,0 +1,46 @@
+from tree import DecisionTree
+from random import choices
+from math import floor
+
+
+def proportional_reproduction(population: list[DecisionTree], fitnesses: list[float]) -> list[DecisionTree]:
+    """
+    Fitness values should be maximized and nonnegative.
+    Trees are not copied; resulting list may contain the same trees as in original list.
+    """
+    return choices(population, fitnesses, k=len(population))
+
+
+def rank_reproduction(population: list[DecisionTree], fitnesses: list[float], a: float, k: float) -> list[DecisionTree]:
+    """
+    Fitness values should be maximized.
+    Trees are not copied; resulting list may contain the same trees as in original list.
+    """
+    sorted_indices = sorted(range(len(population)), key=lambda i: fitnesses[i], reverse=True)
+    sorted_population = [population[i] for i in sorted_indices]
+    weights = [a + k * (1 - rank / len(population)) for rank in range(len(population))]
+    return choices(sorted_population, weights, k=len(population))
+
+
+def truncation_reproduction(population: list[DecisionTree], fitnesses: list[float], rho: float) -> list[DecisionTree]:
+    """
+    Fitness values should be maximized.
+    Trees are not copied; resulting list may contain the same trees as in original list.
+    """
+    sorted_indices = sorted(range(len(population)), key=lambda i: fitnesses[i], reverse=True)
+    truncated_population = [population[i] for i in sorted_indices[:floor(rho * len(population)) + 1]]
+    return choices(truncated_population, k=len(population))
+
+
+def tournament_reproduction(population: list[DecisionTree], fitnesses: list[float], tournament_size: int) -> list[DecisionTree]:
+    """
+    Fitness values should be maximized.
+    Trees are not copied; resulting list may contain the same trees as in original list.
+    """
+    tournament = [None] * tournament_size
+    trees_with_fitness = list(zip(population, fitnesses))
+    result: list[DecisionTree] = []
+    for _ in range(len(population)):
+        tournament = choices(trees_with_fitness, k=tournament_size)
+        result.append(max(tournament, key=lambda tree: tree[1])[0])
+    return result
