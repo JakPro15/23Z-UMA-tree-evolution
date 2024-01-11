e66dc8e2bbd515a7a3672d71c431b5524fd06bdf
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2024-01-11 19:49:15 +0100

    Refactored experiments

---
+++ experiments_evotree.py
@@ -0,0 +1,81 @@
+from evo_tree import EvoTree
+from random import seed
+from helpers import cross_validate, prepare_datasets, RANDOM_STATE
+from multiprocessing import Process
+from reproduction import *
+from succesion import *
+
+
+POPULATION_SIZE = 20
+MAX_ITERATIONS = 500
+
+def leaf_probability(depth):
+    return 1 - (0.5 ** depth)
+
+
+reproductions = [
+    proportional_reproduction,
+    lambda population, fitnesses: rank_reproduction(
+        population, fitnesses, 0.05, get_k_from_a(0.05, POPULATION_SIZE)),
+    lambda population, fitnesses: truncation_reproduction(
+        population, fitnesses, 0.8),
+    lambda population, fitnesses: tournament_reproduction(
+        population, fitnesses, 2),
+]
+
+succesions = [
+    generational_succession,
+    lambda population, genetic_operations_population, scores, genetic_operations_scores: elite_succession(
+        population, genetic_operations_population, scores, genetic_operations_scores, 2),
+]
+
+
+def run_experiment(name, data, counters, file_lengths):
+    for seed_nr in range(25):
+        for max_depth in [5, 20]:
+            for j, reproduction in enumerate(reproductions):
+                for mutation_probability in [0.4, 0.8]:
+                    for leaf_inner_swap_probabilty in [0, 0.3]:
+                        for crossover_probability in [0, 0.4]:
+                            for k, succesion in enumerate(succesions):
+                                counters[name] += 1
+                                if counters[name] <= file_lengths[name]:
+                                    continue
+
+                                seed(seed_nr)
+
+                                model = EvoTree(POPULATION_SIZE, max_depth, leaf_probability, MAX_ITERATIONS, reproduction,
+                                                mutation_probability, leaf_inner_swap_probabilty, crossover_probability, succesion)
+
+                                accuracy, std_dev, min_score, max_score = cross_validate(
+                                    model, data[0], data[1]
+                                )
+
+                                with open(f'./experiment_results/{name}.csv', 'a+') as file:
+                                    file.write(
+                                        f"{seed_nr},{max_depth},{j},{mutation_probability},{leaf_inner_swap_probabilty},"
+                                        f"{crossover_probability},{k},{accuracy},{std_dev},{min_score},{max_score}\n"
+                                    )
+
+if __name__ == "__main__":
+    datasets = prepare_datasets()
+
+    counters = dict([(name, 0) for name in datasets])
+    file_lengths = dict([(name, 0) for name in datasets])
+    for name in datasets:
+        try:
+            file = open(f'./experiment_results/{name}.csv', 'r+')
+            file_lengths[name] = sum([1 for _ in file])
+            file.close()
+        except Exception:
+            file_lengths[name] = 0
+
+    processes = []
+
+    for name, data in datasets.items():
+        process = Process(target=run_experiment, args=(name, data, counters, file_lengths))
+        processes.append(process)
+        process.start()
+
+    for process in processes:
+        process.join()
--- experiments_id3.py
+++ experiments_id3.py
@@ -1,24 +1,13 @@
-from ucimlrepo import fetch_ucirepo
-from sklearn.model_selection import train_test_split
-import pandas as pd
 import six
 import sys
-sys.modules['sklearn.externals.six'] = six
 import numpy as np
+# necessary for id3 library to function
+sys.modules['sklearn.externals.six'] = six
 np.float = float
 from id3 import Id3Estimator
-from helpers import cross_validate, RANDOM_STATE
+from helpers import cross_validate, prepare_datasets
 from multiprocessing import Process
 
-
-def extract_data_uciml(data):
-    target = data.targets.squeeze()
-    attrs = data.features
-
-    x, _, y, _ = train_test_split(
-        attrs, target, test_size=0.2, random_state=RANDOM_STATE)
-
-    return (x, y)
 
 def run_experiment(name, data, counters, file_lengths):
     for seed_nr in range(25):
@@ -28,9 +17,7 @@ def run_experiment(name, data, counters, file_lengths):
                     for gain_ratio in [True, False]:
                         for min_entropy_decrease in [0., 0.3]:
                             for repeating in [True, False]:
-
                                 counters[name] += 1
-
                                 if counters[name] <= file_lengths[name]:
                                     continue
 
@@ -44,26 +31,11 @@ def run_experiment(name, data, counters, file_lengths):
 
                                 with open(f'./experiment_results/id3_{name}.csv', 'a+') as file:
                                     file.write(
-                                        f"{seed_nr},{max_depth},{min_samples_split},{prune},{gain_ratio},{min_entropy_decrease},{repeating},{accuracy},{std_dev},{min_score},{max_score}\n")
+                                        f"{seed_nr},{max_depth},{min_samples_split},{prune},{gain_ratio},{min_entropy_decrease},"
+                                        f"{repeating},{accuracy},{std_dev},{min_score},{max_score}\n")
 
 if __name__ == "__main__":
-    datasets = {}
-
-    datasets["breast_cancer"] = extract_data_uciml(fetch_ucirepo(id=17).data)
-    datasets["dry_bean"] = extract_data_uciml(fetch_ucirepo(id=602).data)
-    datasets["glass"] = extract_data_uciml(fetch_ucirepo(id=42).data)
-    datasets["wine"] = extract_data_uciml(fetch_ucirepo(id=109).data)
-
-    data = pd.read_csv('./datasets/high_diamond_ranked_10min.csv')
-
-    target = data['blueWins']
-
-    attrs = data.drop(columns=['gameId', 'blueWins'])
-
-    x, _, y, _ = train_test_split(
-        attrs, target, test_size=0.2, random_state=RANDOM_STATE)
-
-    datasets["lol"] = (x, y)
+    datasets = prepare_datasets()
 
     counters = dict([(name, 0) for name in datasets])
     file_lengths = dict([(name, 0) for name in datasets])
