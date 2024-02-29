18386a663f34dde3d93e915c965b52e90ba57b97
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2024-02-29 18:48:01 +0100

    Removed plot images and results CSVs

--- final_docs_assets/plots/convert.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import os
-
-for name in os.listdir("."):
-    if "confusion_" == name[:len("confusion_")]:
-        im = Image.open(name)
-        im = im.crop((37, 0, im.size[0] - 78, im.size[1] - 1))
-        im.save(f'{name}')
